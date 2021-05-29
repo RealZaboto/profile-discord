5b13433dea27fe9cfc4e0152156c74bf7fa5cf5a
RealZaboto <83482500+RealZaboto@users.noreply.github.com>
2021-05-29 16:02:33 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,24 @@
+const config = {
+    
+    USERNAME: "FunTazz#0001",
+    PICTURE: "https://cdn.discordapp.com/avatars/549204759500816394/a_1672253c8bffc9d585b8c282c4d3158b.gif?size=1024",
+
+    DESCRIPTION: "Étudiant Français, passionné par l'Informatique et Discord, je travaille la plupart de mon temps sur L'Empire du Giveaway et Funny Place.    ",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "https://discord.com/assets/64ae1208b6aefc0a0c3681e6be36f0ff.svg" },
+        { id: "Nitro Sub", image: "https://discord.com/assets/386884eecd36164487505ddfbac35a9d.svg" },
+        { id: "Nitro Sub", image: "https://discord.com/assets/4c380650960c2b1e1584115d5e9ad63b.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.gg/Ya6nSb9", name: "L'empire du Giveaways" },
+        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.com", name: "Funny Place" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/channel/UCAiA0_NmYh-BE4Q2DeSxi-w" },
+        { class_name: "fab fa-tiktok", link: "https://www.tiktok.com/@funtazz?lang=fr" },
+    ],
+
+}
